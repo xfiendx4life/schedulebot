e66fec2fa1dcfdee16ccc87aaa10fb70a1cf9f7c
Kirill Ivanov <xfiendx4life@gmail.com>
2016-08-08 00:14:47 +0300

    Added db update function

---
+++ db_processing.py
@@ -0,0 +1,17 @@
+import sqlite3
+def create_db():
+    conn = sqlite3.connect('schools.db')
+    c = conn.cursor()
+    c.execute('CREATE TABLE schools (chat_id text, school_id text)')
+    conn.close()
+    
+def db_update(chat_id, sc_id):
+    conn = sqlite3.connect('schools.db')
+    c = conn.cursor()
+    c.execute('SELECT school_id FROM schools WHERE chat_id = %s' % chat_id)
+    if c.fetchone():
+        c.execute('UPDATE schools SET school_id=%s' % sc_id)
+    else:
+        c.execute('INSERT INTO schools VALUES (%s, %s)' % (chat_id, sc_id))
+    conn.commit()
+    conn.close()
