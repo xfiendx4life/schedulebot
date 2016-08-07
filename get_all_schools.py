67a78b024e9fd523ee43c04fa2f186a61d45e638
Kirill Ivanov <xfiendx4life@gmail.com>
2016-08-07 20:57:03 +0300

    Added functions to get city id and school_list by city name

---
+++ get_all_schools.py
@@ -0,0 +1,35 @@
+#https://sgo.volganet.ru/lacc.asp?Function=GetCityList2&StateID=34
+#https://sgo.volganet.ru/lacc.asp?Function=GetSchools&city=216
+import requests
+import xml.etree.ElementTree  as ET
+
+def get_city_list():
+    r = requests.post('https://sgo.volganet.ru/lacc.asp?Function=GetCityList2&StateID=34')
+    #r.encoding = 'utf-8'
+    root = ET.fromstring(r.text)
+    city_list = []
+    for child in root[0]:
+        city_dict = {}
+        city_dict['cityid'] = child.find('cityid').text
+        city_dict['city'] = child.find('city').text
+        city_list.append(city_dict)
+    return city_list
+    
+def get_city_id(name):
+    lst = get_school_list()
+    for item in lst:
+        if item['city'] == name:
+            return item['cityid']
+
+def get_school_list(cityid):
+    r = requests.post('https://sgo.volganet.ru/lacc.asp?Function=GetSchools&city=%s' % cityid)
+    root = ET.fromstring(r.text)
+    school_list = []
+    for child in root[0]:
+        school_dict = {}
+        school_dict['schoolid'] = child.find('schoolid').text
+        school_dict['schoolname'] = child.find('schoolname').text
+        school_list.append(school_dict)
+    return school_list
+
+
